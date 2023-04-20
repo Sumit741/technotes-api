703cb0c59faddbfd52ce048d548540591d12ceff
Sumit700 <sumit.sunar@innovatetech.co>
2023-04-20 16:22:16 +0545

    Ready to deploy

---
+++ config/corsOptions.js
@@ -0,0 +1,15 @@
+const allowedOrigins = require("./allowedOrigins");
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true); //first is error, and second is success
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
